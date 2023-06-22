84bc926b9660f1839df46c0dc68d0033206519cf
GCarrion <52300475+gecmvoz@users.noreply.github.com>
2023-06-22 00:55:05 -0600

    accessjs

---
+++ JAVASCRIPT/accesorio.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("navBar");
+    if (x.className === "navBar") {
+        x.className = "responsive";
+        document.getElementsByClassName("responsive").style.height = "100%";
+        document.getElementById("mobileicon").style.display = "none";
+    } else {
+        x.className = "navBar";   
+        document.getElementById("responsive").style.height = "0%";
+      }
+  }
